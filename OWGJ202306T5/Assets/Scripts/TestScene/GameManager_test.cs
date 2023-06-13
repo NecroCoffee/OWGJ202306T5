ed1cd68d6cc907e365a3f66d0b8e6867057779a3
NecroCoffee <ikasumi.514@i.softbank.jp>
2023-06-14 02:18:32 +0900

    少し進んだの

--- OWGJ202306T5/Assets/Scripts/TestScene/GameManager_test.cs
+++ OWGJ202306T5/Assets/Scripts/TestScene/GameManager_test.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager_test : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int player_Life = 15;//プレイヤー残機
+
+    public Vector2 save_tmp;//セーブオブジェクト位置保存用
+
+    public bool Is_SaveObjectActive;//セーブオブジェクトは有効かどうか
+
+    
+    private void Player_Action_Reset()//リセット処理
+    {
+
+    }
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
         
+    }
+
+    private void Update()
+    {
+        save_tmp = GameObject.FindWithTag("SaveObject").transform.position;//セーブオブジェクトの位置を取得；
+        //Debug.Log("tmp.x;" + Save_tmp.x);
+        //Debug.Log("tmp.y;" + Save_tmp.y);
+
     }
 }
